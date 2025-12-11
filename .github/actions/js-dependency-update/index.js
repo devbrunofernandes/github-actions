9764967ff0b3b42598159f359ab0be9f3e3ffaa8
Bruno Vilas Boas Fernandes <brunovbf2006@gmail.com>
2025-12-11 16:00:18 -0300

    Add logic to index.js in js-dependency-update-check action

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,11 @@
 const core = require('@actions/core');
- 
+const exec = require('@actions/exec');
+
+const validateBranchName = ({ branchName }) =>
+  /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) =>
+  /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+
 async function run() {
   /*
   1. Parse inputs:
@@ -15,6 +21,57 @@ async function run() {
   5. Otherwise, conclude the custom action.
   */
   core.info('I am a custom JS action');
+
+  // inputs
+  const base_branch = core.getInput('base-branch');
+  const target_branch = core.getInput('target-branch');
+  const working_directory = core.getInput('working-directory');
+  const gh_token = core.getInput('gh-token');
+  const debug = core.getBooleanInput('debugs');
+
+  core.setSecret(gh_token);
+
+  // validating
+  if (!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(
+      'Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    core.setFailed(
+      'Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateDirectoryName({ dirName: workingDir })) {
+    core.setFailed(
+      'Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.'
+    );
+    return;
+  }
+
+  // print the values in the screen
+  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+  // executing commands in shell
+  await exec.exec('npm update', [], {
+    cwd: working_directory,
+  });
+
+  const git_status_result = await exec.getExecOutput('git status -s package*.json', [], {
+    cwd: working_directory,
+  });
+
+  if (git_status_result.stdout.length > 0) {
+    core.info('[js-dependency-update]: There are some updates avaliable.');
+  } else {
+    core.info('[js-dependency-update]: No updates avaliable.');
+  }
 }
 
 run();
